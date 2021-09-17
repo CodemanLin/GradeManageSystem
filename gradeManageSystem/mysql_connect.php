bc27456c743452ce08bada9d2ff03c97d83b033f
“yourlinqinzao“ <799548225@qq.com>
2021-09-17 23:51:27 +0800

    update

--- gradeManageSystem/mysql_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = @mysqli_connect("localhost","root","");
-$conn or die('数据库服务器连接失败！系统错误信息为：'.mysqli_connect_error());
-mysqli_select_db($conn, "stugrade") 
-or die('打开数据库失败！系统错误信息为：'.mysqli_error($conn));
-mysqli_query($conn, "set names utf8");
-?>
